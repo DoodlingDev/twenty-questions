9556b4d52a8ae1c657234496335189da21b7cd5a
DoodlingDev <aji.slater@gmail.com>
2018-02-07 14:15:13 -0600

    Adds further tests for more complicated schemas

--- test/components/validator.spec.js
+++ test/components/validator.spec.js
@@ -51,8 +51,6 @@ describe("internal state after constructor", () => {
     const objectKeys = Object.keys(wrapper.state().validationList);
     expect(typeof wrapper.state().validationList).toEqual("object");
     expect(objectKeys).not.toContain("0");
-
-
   });
 });
 
@@ -62,6 +60,139 @@ describe("gatherValidations", () => {
       const wrapper = setup(shallow, oneValidation);
       const validationList = wrapper.state().validationList;
       expect(Object.keys(validationList).length).toBe(1);
+    });
+
+    it("should have the correct shape", () => {
+      const wrapper = setup(shallow, oneValidation);
+      const validationList = wrapper.state().validationList;
+      expect(validationList).toEqual({
+        "one": {
+          type: "string",
+          label: "ONE",
+          name: "one",
+          validates: ["--one"],
+        },
+      });
+    });
+  });
+
+  describe("given two sibling validations", () => {
+    const twoSiblingsToValidate = {
+      schema: {
+        type: "object",
+        name: "parent",
+        properties: [
+          {
+            name: "one",
+            type: "string",
+            label: "ONE",
+            validates: ["--one"],
+          },
+          {
+            name: "two",
+            type: "string",
+            label: "TWO",
+            validates: ["--one", "--two"],
+          },
+        ],
+      },
+    };
+
+    it("should return two fields to validate", () => {
+      const wrapper = setup(shallow, twoSiblingsToValidate);
+      const validationList = wrapper.state().validationList;
+      expect(Object.keys(validationList).length).toBe(2);
+    });
+
+    it("should be shaped properly", () => {
+      const wrapper = setup(shallow, twoSiblingsToValidate);
+      const validationList = wrapper.state().validationList;
+      expect(validationList).toEqual({
+        "one": {
+          name: "one",
+          label: "ONE",
+          type: "string",
+          validates: ["--one"],
+        },
+        "two": {
+          name: "two",
+          label: "TWO",
+          type: "string",
+          validates: ["--one", "--two"],
+        },
+      });
+    });
+  });
+
+  describe("given nested properties", () => {
+    const nestedProperties = {
+      schema: {
+        title: "test schema",
+        type: "object",
+        properties: [
+          {
+            type: "string",
+            name: "one",
+            label: "ONE",
+            validates: ["--one"],
+          },
+          {
+            type: "object",
+            name: "object-1",
+            properties: [
+              {
+                type: "string",
+                name: "two",
+                label: "TWO",
+                validates: ["--two"],
+              },
+              {
+                type: "object",
+                name: "object-2",
+                properties: [
+                  {
+                    type: "number",
+                    name: "three",
+                    label: "THREE",
+                    validates: ["--one", "--two", "--three"],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    };
+
+    it("should return three fields to validate", () => {
+      const wrapper = setup(shallow, nestedProperties);
+      const validationList = wrapper.state().validationList;
+      expect(Object.keys(validationList).length).toBe(3);
+    });
+
+    it("should have the correct shape", () => {
+      const wrapper = setup(shallow, nestedProperties);
+      const validationList = wrapper.state().validationList;
+      expect(validationList).toEqual({
+        one: {
+          type: "string",
+          name: "one",
+          label: "ONE",
+          validates: ["--one"],
+        },
+        two: {
+          type: "string",
+          name: "two",
+          label: "TWO",
+          validates: ["--two"],
+        },
+        three: {
+          type: "number",
+          name: "three",
+          label: "THREE",
+          validates: ["--one", "--two", "--three"],
+        },
+      });
     });
   });
 });
