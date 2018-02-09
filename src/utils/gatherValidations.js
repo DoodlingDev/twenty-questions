71dd6eb9e1a44fc84aca4f412d364f83048867d1
DoodlingDev <aji.slater@gmail.com>
2018-02-09 13:57:55 -0600

    Renames validator.js from utils more appropriately & tests
    The tests already existed for gatherValidations, I believe they were
    removed for expediency in testing for a change in another branch and the
    commented out tests got committed anyway.
    
    Also because the validator is a component now instead of being wholly a
    module, it didn't make sense for this file to remain being called
    "validator.js", so renamend to match its actual contents more closely.

--- src/utils/gatherValidations.js
+++ src/utils/gatherValidations.js
@@ -9,7 +9,7 @@
  * @param {q20$Node[]} formNode the passed in schema from FormController
  * @return {q20$ValidationList} an array built of validation objects
  */
-export function gatherValidations(formNode: q20$Node): q20$ValidationList {
+export default function gatherValidations(formNode: q20$Node): q20$ValidationList {
   let outputBuffer = {};
 
   for (let property in formNode) {
---
+++ test/utils/gatherValidations.spec.js
@@ -0,0 +1,140 @@
+import gatherValidations from "../../src/utils/gatherValidations";
+
+describe("gatherValidations", () => {
+  describe("given one non-nested validation", () => {
+    const form = {
+      name: "name",
+      path: "name.path",
+      valueManager: {},
+      label: "label-string",
+      type: "string",
+      validates: ["one"],
+      properties: [
+        {
+          type: "string",
+          name: "don't validate me",
+        },
+      ],
+    };
+
+    it("should return one field to validate", () => {
+      expect(Object.keys(gatherValidations(form)).length).toBe(1);
+    });
+
+    it("should return a properly formatted object", () => {
+      expect(gatherValidations(form)).toEqual(
+        {
+          "name": {
+            type: "string",
+            name: "name",
+            label: "label-string",
+            validates: ["one"],
+          },
+        }
+      );
+    });
+  });
+
+  describe("given three siblings: 2 validated, 1 not", () => {
+    const form = {
+      name: "one",
+      label: "ONE",
+      type: "object",
+      properties: [
+        {
+          type: "string",
+          name: "sibling-one",
+          label: "sibling one",
+          validates: ["--one"],
+        },
+        {
+          type: "string",
+          name: "sibling-two",
+          label: "sibling two",
+          validates: ["--one", "--two"],
+        },
+        {
+          type: "string",
+          name: "sibling-three",
+          label: "sibling three",
+        },
+      ],
+    };
+
+    it("should return two fields to validate", () => {
+      const result = gatherValidations(form);
+      expect(Object.keys(result).length).toBe(2);
+    });
+
+    it("should correctly populate the validation objects", () => {
+      const result = gatherValidations(form);
+      expect(result).toEqual({
+        "sibling-one": {
+          type: "string",
+          name: "sibling-one",
+          label: "sibling one",
+          validates: ["--one"],
+        },
+        "sibling-two": {
+          type: "string",
+          name: "sibling-two",
+          label: "sibling two",
+          validates: ["--one", "--two"],
+        },
+      });
+    });
+  });
+
+  describe("given several layers of nesting", () => {
+    const form = {
+      type: "object",
+      name: "row_1",
+      label: "row 1",
+      properties: [
+        {
+          type: "object",
+          name: "row_2",
+          label: "row 2",
+          properties: [
+            {
+              type: "object",
+              name: "row_3",
+              label: "row 3",
+              properties: [
+                {
+                  type: "string",
+                  name: "string-3",
+                  label: "string 3",
+                  validates: ["--three"],
+                },
+                {
+                  type: "string",
+                  name: "string-4",
+                  label: "string 4",
+                  validates: ["--four"],
+                },
+              ],
+            },
+            {
+              type: "string",
+              name: "string-2",
+              label: "string 2",
+              validates: ["--two"],
+            },
+          ],
+        },
+        {
+          type: "string",
+          name: "string-1",
+          label: "string 1",
+          validates: ["--one"],
+        },
+      ],
+    };
+
+    it("should return four fields to validate", () => {
+      const result = gatherValidations(form);
+      expect(Object.keys(result).length).toBe(4);
+    });
+  });
+});
