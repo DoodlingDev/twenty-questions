ace9001aa209aa1ccd7309f55e525edbbfdf8142
DoodlingDev <aji.slater@gmail.com>
2018-02-12 14:17:33 -0600

    Moved deciding which node into the object node
    Changed the main node layer to the object node.
    There was a very uneccessary step in the FormNode component just routing
    through to which node should handle the field based on the type. The
    object node can handle that, because the object node only renders other
    nodes, there isn't much else going on there.
    
    Objects also feel like where the other nodes should be travelling
    through anyway. It's the holder of all the ndoe data, and the properties
    themselves.

--- src/components/nodes/types/object.js
+++ src/components/nodes/types/object.js
@@ -1,15 +1,41 @@
 // @flow
 import React from "react";
-import FormNode from "../../formNode";
+import FormNodeString from "./string";
+import FormNodeBoolean from "./boolean";
+import FormNodeNumber from "./number";
+//import FormNodeArray from "./array";
 
 export const FormNodeObject = (props: q20$RenderedNode) => {
+  if (props.register) {
+    props.register(props.path);
+  }
+  const nodeTypes = {
+    string: FormNodeString,
+    boolean: FormNodeBoolean,
+    number: FormNodeNumber,
+    // array: FormNodeArray,
+    object: FormNodeObject,
+  };
+
   let nodeContent = null;
   if (props.properties) {
     nodeContent = props.properties.map((property: q20$Node) => {
+      if (!Object.keys(nodeTypes).includes(property.type)) {
+        throw new Error(
+          "FormNode was supplied an invalid type." +
+            ` The type ${property.type} is not valid.`,
+        );
+      }
+
+      const nodePath = `${props.path}.${property.name}`;
+      if (props.register) {
+        props.register(nodePath);
+      }
+      const NodeComponent = nodeTypes[property.type];
       return (
-        <FormNode
+        <NodeComponent
           key={`node-${props.path}.${property.name}`}
-          path={`${props.path}`}
+          path={nodePath}
           widgets={props.widgets}
           valueManager={props.valueManager}
           register={props.register}
