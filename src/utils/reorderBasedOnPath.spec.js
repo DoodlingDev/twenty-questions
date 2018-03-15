dc5f091d9c33c93d1ac05ed0749e0aab0ea9e05c
DoodlingDev <aji.slater@gmail.com>
2018-03-15 14:03:14 -0500

    corrects error in array delete functionality

--- src/utils/reorderBasedOnPath.spec.js
+++ src/utils/reorderBasedOnPath.spec.js
@@ -76,26 +76,21 @@ describe("reorderBasedOnPath", () => {
     });
     expect(Object.keys(result).length).toBe(Object.keys(arrayValue).length);
   });
-});
 
-describe("areConsecutive", () => {
-  it("returns true when the array of numbers is consecutive", () => {
-    const numbers = [1, 2, 3, 4, 5];
-    expect(areConsecutive(numbers)).toBeTruthy();
-  });
-
-  it("returns false when the array of numbers is not consecutive", () => {
-    const numbers = [1, 2, 3, 9, 10];
-    expect(areConsecutive(numbers)).toBe(false);
-  });
-
-  it("returns true when consecutive but not starting at zero", () => {
-    const numbers = [21, 22, 23, 24];
-    expect(areConsecutive(numbers)).toBe(true);
-  });
-
-  it("returns false when there is only one value", () => {
-    const numbers = [1];
-    expect(areConsecutive(numbers)).toBe(false);
+  it("should remove a duplicate row if it exists after reorder", () => {
+    const arrayValue = {
+      "testFormTitle.testName.testArray": {},
+      "testFormTitle.testName.testArray.1.a": {},
+      "testFormTitle.testName.testArray.1.a.one": "AAA",
+      "testFormTitle.testName.testArray.1.a.two": "BBB",
+      "testFormTitle.testName.testArray.2.a": {},
+      "testFormTitle.testName.testArray.2.a.one": "AAAA",
+      "testFormTitle.testName.testArray.2.a.two": "BBBB",
+    };
+    const result = reorderBasedOnPath({
+      path: "testFormTitle.testName.testArray",
+      state: arrayValue,
+    });
+    expect(Object.keys(result).length).toBe(Object.keys(arrayValue).length);
   });
 });
